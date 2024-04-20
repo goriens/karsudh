fa18e68e3b96ff94670b1ee409c00a42338c4c11
Suhaib Gour <76823175+goriens@users.noreply.github.com>
2024-04-21 00:22:58 +0530

    all

--- assets/mail/contact-form.php
+++ assets/mail/contact-form.php
@@ -1,42 +1,33 @@
 <?php
-$conName = $_POST['conName'];
-$conEmail = $_POST['conEmail'];
-$conMessage = $_POST['conMessage'];
+// Check if the request method is POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+   $conName = $_POST['conName'];
+   $conEmail = $_POST['conEmail'];
+   $conMessage = $_POST['conMessage'];
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+   ini_set('display_errors', 1);
+   error_reporting(E_ALL);
 
-/**
- * Set the recipient email address.
- * 
- * FIXME: Update this to your desired email address.
- */
-$recipient = "sohaibgoriens@gmail.com";
+   $recipient = "contact@karsudh.com";
+   $sender = $conName . " { " . $conEmail . " }";
+   $head = "You have a new message from your Webency website Contact Form\n=============================";
+   $form_content = "$head\n\n";
+   $form_content .= "Full Name: $conName\n";
+   $form_content .= "Email: $conEmail\n";
+   $form_content .= "Message: \n$conMessage\n";
+   $headers = "From: $conName < $conEmail >\r\n" .
+               "Reply-To:" . $conEmail . "\r\n" .
+               'X-Mailer: PHP/' . phpversion();
 
-// Set the email subject.
-$sender = $conName . " { " . $conEmail . " }";
-
-
-//Email Header
-$head = "You have a new message from your Webency website Contact Form\n=============================";
-
-// Build the email content.
-$form_content = "$head\n\n";
-
-$form_content .= "Full Name: $conName\n";
-
-$form_content .= "Email: $conEmail\n";
-
-$form_content .= "Message: \n$conMessage\n";
-
-
-// Build the email headers.
-$headers = "From: $conName < $conEmail >\r\n" .
-  "Reply-To:" . $conEmail . "\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-
-if (mail($recipient, $sender, $form_content, $headers)) {
-  echo "Y";
+   if (mail($recipient, $sender, $form_content, $headers)) {
+       echo "Y";
+   } else {
+       echo "N";
+   }
 } else {
-  echo "N";
+   // If the request method is not POST, return an error message
+   http_response_code(405); // Set the HTTP response code to 405 (Method Not Allowed)
+   echo "Error: This script can only be accessed via a POST request.";
+   exit;
 }
+?>
